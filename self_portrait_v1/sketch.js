a2b91422dbe135307d3f97cc504e5c20a5ce5eac
danmunoz111 <70981075+danmunoz111@users.noreply.github.com>
2020-09-15 17:31:52 -0400

    Self Portrait Version 1

---
+++ self_portrait_v1/sketch.js
@@ -0,0 +1,36 @@
+/*
+self portrait v1
+by Daniel Munoz
+*/
+
+function setup() {
+	createCanvas(640,360);
+}
+
+function draw() {
+	background('blue');
+//face
+fill(145,89,133)
+circle(480,110,200,);
+//eyes
+fill(254,56,123)
+	circle(400,100,50);//left
+	circle(520,100,50);//right
+//mouth
+	rect(440,150,100,50,10);
+
+//hair
+line(400,40,100,210)
+line(420,40,110,220)
+line(430,40,140,230)
+line(450,40,150,240)
+line(470,40,100,250)
+line(500,40,110,260)
+line(510,40,620,210)
+line(510,40,620,210)
+line(520,40,630,210)
+line(530,40,640,210)
+
+
+
+}
