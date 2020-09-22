661fc7fd10a940a7edad684e55ad65e28a092555
danmunoz111 <70981075+danmunoz111@users.noreply.github.com>
2020-09-22 17:44:40 -0400

    Variables

---
+++ self_portrait_v2/sketch.js
@@ -0,0 +1,47 @@
+/*
+self portrait v1
+by Daniel Munoz
+*/
+// global scope
+
+var x = 480; // statement
+var y = 100
+var eyeSize =100
+var mouthSize=eyeSize;
+
+var faceRed = 145
+var faceBlue= 133
+var faceGreen= 89
+
+
+function setup() {
+	createCanvas(640,360);
+}
+
+function draw() {
+	background('blue');
+//face
+fill(faceRed,faceGreen,faceBlue)
+circle(x,y+10,200,);
+//eyes
+fill(254,56,123)
+	circle(x-80,y,50);//left
+	circle(x+40,y,50);//right
+//mouth
+	rect(x-40,y+50,mouthSize,mouthSize/2,mouthSize/10);
+
+//hair
+line(x-80,y-60,x-380,y+110)
+line(x-60,y-60,x-370,y+120)
+line(x-50,y-60,x-340,y+130)
+line(x-30,y-60,x-330,y+140)
+line(x-10,y-60,x-380,y+150)
+line(x+20,y-60,x-370,y+160)
+line(x+30,y-60,x+140,y+110)
+line(x+30,y-60,x+140,y+110)
+line(x+30,y-60,x+150,y+110)
+line(x+50,y-60,x+160,y+110)
+
+
+
+}
